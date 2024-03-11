bc0a9080d400cdbc6678bb96a0339c0276ac36da
Vlaeminck <tomasvlaeminck.info@gmail.com>
2024-03-10 23:07:35 -0300

    Lógica agregada + Footer + Cambios en CSS

---
+++ script.js
@@ -0,0 +1,66 @@
+const textarea = document.querySelector(".textarea");
+const mensaje = document.querySelector(".mensaje");
+
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textarea.value);
+    mensaje.value = textoEncriptado;
+    textarea.value = "";
+    mensaje.style.backgroundImage = "none";
+    
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textarea.value);
+    mensaje.value = textoEncriptado;
+    textarea.value = "";
+    mensaje.style.backgroundImage = "none";
+  
+
+}
+
+function toggleModoOscuro() {
+    const body = document.body;
+    body.classList.toggle('modo-oscuro-activo');
+}
+
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptado;
+}
+
+function desencriptar(stringDesencriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptado;
+}
+
+function copiarTexto() {
+    const mensaje = document.querySelector('.mensaje');
+    mensaje.select();
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
+
+window.onload = function() {
+    // Limpiar el contenido de .mensaje al cargar la página
+    mensaje.value = "";
+}
